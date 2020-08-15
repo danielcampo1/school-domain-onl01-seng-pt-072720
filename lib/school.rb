379f50f784725e369901f3987045a00348378efa
daniel campo <campo_daniel94@yahoo.com>
2020-08-15 15:45:57 +0000

    Automatically backed up by Learn

---
+++ lib/school.rb
@@ -0,0 +1,14 @@
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = []
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+
+end
