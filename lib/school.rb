746562b3c65133db3572c32c210bed7174126e3d
daniel campo <campo_daniel94@yahoo.com>
2020-08-15 15:47:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,25 @@
 class School
-  attr_accessor :name, :roster
-
+  attr_accessor :school
+  attr_reader :roster
+  
   def initialize(name)
-    @name = name
+    @name = name 
     @roster = []
-  end
-
+  end   
+  
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
-  end
-
+    roster[grade] ||= []
+    roster[grade] << "#{name}" 
+  end   
+  
+  def grade(grade_num)
+    roster[grade_num] 
+  end   
+  
+  def sort
+    roster = roster.sort_by { |k, v| k }
+    roster.each do |grade, name|
+      #sort names in each grade
+    end
+  end 
 end
